008d6a780b157e2fe845a2b8f10064ce03f4d098
stewart <stewart@escea.com>
2025-06-17 12:29:42 +1200

    Use async runtime

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
+use std::time::Duration;
+
 use evdev::Device;
-fn main() {
+use tokio::{signal, time::sleep};
+use tokio_util::sync::CancellationToken;
+
+#[tokio::main]
+async fn main() -> Result<(), tokio::io::Error> {
     println!("Hello, world!");
-    print_keys();
+
+    let token = CancellationToken::new();
+
+    let print_thread = tokio::spawn(print_keys(token.clone()));
+
+    match signal::ctrl_c().await {
+        Ok(_) => {
+            token.cancel();
+            print_thread.await?;
+        },
+        Err(err) => {
+            eprintln!("{}", err);
+            return Err(err);
+        },
+    }
+
+    Ok(())
+    
 }
 
-fn print_keys() {
+async fn print_keys(token: CancellationToken) {
     let device_id = "/dev/input/by-id/usb-SIGMACHIP_USB_Keyboard-event-kbd";
     #[allow(unused)]
     let device_redirect = "/dev/packingkeyboard";
@@ -28,7 +51,12 @@ fn print_keys() {
             }
         };
         println!("{:#?}", keys);
+        sleep(Duration::from_secs(1)).await;
+        match token.is_cancelled() {
+            true => break,
+            false => {},
+        }
     }
-    
+
     let _ = device.ungrab();
 }
