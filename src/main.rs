94f1a7b34615192e6bb2b5aa5f17f9f203ea38ff
stewart <stewart@escea.com>
2025-06-17 12:23:53 +1200

    Loop

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ fn main() {
     print_keys();
 }
 
-
-
 fn print_keys() {
     let device_id = "/dev/input/by-id/usb-SIGMACHIP_USB_Keyboard-event-kbd";
     #[allow(unused)]
@@ -15,21 +13,22 @@ fn print_keys() {
         Err(err) => {
             eprintln!("{}", err);
             return;
-        },
+        }
     };
-    
 
     let _ = device.grab();
 
-    let keys = match device.get_key_state() {
-        Ok(k) => k,
-        Err(err) => {
-            eprintln!("{}", err);
-            let _ = device.ungrab();
-            return;
-        },
-    };
-
-    println!("{:#?}", keys);
+    loop {
+        let keys = match device.get_key_state() {
+            Ok(k) => k,
+            Err(err) => {
+                eprintln!("{}", err);
+                let _ = device.ungrab();
+                return;
+            }
+        };
+        println!("{:#?}", keys);
+    }
+    
     let _ = device.ungrab();
 }
